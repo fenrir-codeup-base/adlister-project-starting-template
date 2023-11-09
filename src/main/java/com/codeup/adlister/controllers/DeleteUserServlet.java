945e3ca6386e6421fe0e49ccc6c89d2e4463f563
Jeremiah Matthews <jeremiah.matthews105@gmail.com>
2023-11-09 13:02:38 -0600

    updated delete servlet

--- src/main/java/com/codeup/adlister/controllers/DeleteUserServlet.java
+++ src/main/java/com/codeup/adlister/controllers/DeleteUserServlet.java
@@ -1,4 +1,33 @@
 package com.codeup.adlister.controllers;
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+import java.io.IOException;
+import java.sql.Connection;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet(name = "DeleteUserServlet", urlPatterns = "/deleteuser")
+public class DeleteUserServlet extends HttpServlet {
 
-public class DeleteUserServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/users/deleteuser.jsp").forward(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        // Retrieve the ID of the user to delete from the user and delete the parameters
+        Object user;
+        User loggedInUser = (User) request.getSession().getAttribute("user");
+        // delete user
+        DaoFactory.getUsersDao().delete(loggedInUser);
+        request.getSession().removeAttribute("user");
+        // Method used to clear the session after deleting the user
+        request.getSession().invalidate();
+        // Redirect to the login page
+        Object login;
+        response.sendRedirect("/login");
+    }
 }
